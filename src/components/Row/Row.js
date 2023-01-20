e4fb8b2ababe6af28017c3caf3d68acd04a5e7d4
SamiAbar30 <samiabar30@gmail.com>
2023-01-20 16:14:53 +0100

    build

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,27 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { axiosConfig } from '../../config/axios/axiosConfig';
-
+import "./Row.css"
 export default function Row({ title, fetchUrl, isLargRow = false }) {
 	const [items, setItems] = useState();
 	const base_url = 'https://image.tmdb.org/t/p/original/';
 	useEffect(() => {
 		return async () => {
 			const request = await axiosConfig.get(fetchUrl);
+      console.log(request.data.results);
 			setItems(request.data.results);
 		};
 	}, []);
 	return (
 		<div className='text-white ml-5'>
 			<h2>{title}</h2>
-			<div className=''>
-				{items.map((Item) => (
+			<div className='flex overflow-y-hidden overflow-x-scroll p-5 row__posters '>
+				{items?.map((item) => (
 					<img
-						key={Item.id}
-						className={` ${isLargRow}`}
-						src={`${base_url}${
-							isLargRow ? items.poster_path : items.backdrop_path
+						key={item.id}
+						className={` object-contain mr-2.5 w-full transition transform duration-[450ms] hover:scale-110 hover:opacity-100 ${isLargRow ?' max-h-64':'max-h-28'}`}
+						src={`${base_url}${isLargRow ? item?.poster_path : item?.backdrop_path
 						}`}
-						alt={Item.name}
+						alt={item.name}
 					/>
 				))}
 			</div>
--- src/pages/home-page/index.js
+++ src/pages/home-page/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import Banner from '../../components/banner/Banner';
 import NavigationBar from '../../components/navigation-bar/NavigationBar';
+import Row from '../../components/Row/Row';
 import requests from '../../config/api/links';
 export default function HomeScreen() {
 	return (
 		<>
 			<NavigationBar/>
 			<Banner/>
-			{/* <Row title="Trending Now" fetchUrl={requests.fetchAllTrending} />
-			<Row title="Trending Now" fetchUrl={requests.fetchAllTrending} />
-			<Row title="Trending Now" fetchUrl={requests.fetchAllTrending} />
-			<Row title="Trending Now" fetchUrl={requests.fetchAllTrending} />
-			<Row title="Trending Now" fetchUrl={requests.fetchAllTrending} />
-			<Row title="Trending Now" fetchUrl={requests.fetchAllTrending} />
-			<Row title="Trending Now" fetchUrl={requests.fetchAllTrending} />
-			<Row title="Trending Now" fetchUrl={requests.fetchAllTrending} />
-			 */}
+			<Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargRow={true} />
+          <Row title="Trending Now" fetchUrl={requests.fetchAllTrending}  />
+          <Row title="Top Rated" fetchUrl={requests.fetchTopRated}  />
+          <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}  />
+          <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}  />
+          <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}  />
+          <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}  />
+          <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+			
 		</>
 	);
 }
