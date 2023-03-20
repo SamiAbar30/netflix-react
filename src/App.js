4f57b3a6579968f45340b7bc0328e92c9d327a8f
SamiAbar30 <samiabar30@gmail.com>
2023-03-20 10:29:36 +0000

    fix login layout

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 import Login from './components/login/Login';
 import AuthScreen from './pages/auth';
 function App() {
-	const user=true;
+	const user=false;
 	return (
 		<>
 		{user?<Outlet/>:<AuthScreen/>}
--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -3,11 +3,14 @@ import React from 'react'
 export default function Signup() {
   return (
     <div className='max-width-80 p-[70px] mx-auto bg-black/[0.85]'>
-      <form className='grid flex-col'>
-        <h1>Sign In</h1>
-        <input placeholder='Email' type='email' className='p-5 outline-0 h-8 w-1/3 border-none max-width-[]' />
-        <input placeholder='Password' type='password' className=''/>
-        <buttion type="submit">Sig In</buttion>
+      <form className=' flex-col grid gap-4 justify-items-center'>
+        <input placeholder='Email' type='email' className='p-5 outline-0 h-8  border-none w-1/3' />
+        <input placeholder='Password' type='password' className='p-5 outline-0 h-8 border-none w-1/3'/>
+       <div className='w-52 flex justify-between'>
+       <buttion type="submit">Log In</buttion>
+       <buttion>Sig In</buttion>
+       </div> 
+
       </form>
     </div>
   )
