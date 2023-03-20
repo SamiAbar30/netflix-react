c3146ccb5d8403858c2cd40ae0939f40963ab86b
SamiAbar30 <samiabar30@gmail.com>
2023-03-20 10:01:11 +0000

    fix reute

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 import Login from './components/login/Login';
 import AuthScreen from './pages/auth';
 function App() {
-	const user=false;
+	const user=true;
 	return (
 		<>
 		{user?<Outlet/>:<AuthScreen/>}
